d921ff381fb5b436bd420602aa3e060ae06bbfea
Uruma <143082079+SowuBSBso@users.noreply.github.com>
2025-07-29 19:13:53 +0200

    Add files via upload

---
+++ Config/Config.py
@@ -0,0 +1,28 @@
+name_tool       =  "Uruma DEVICE"
+type_tool       =  "Multi-Tools"
+version_tool    =  "6.6"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "Uruma"
+platform        =  "Windows 10/11 & Linux"
+website         =  "None"
+github_tool     =  ""
+telegram        =  ""
+gunslol         =  ""
+url_config      =  ""
+url_downloads   =  ""
+license         =  ""
+copyright       =  ""
+
+# Couleurs ANSI si pas déjà définies dans Config.Util ou Config.Config
+try:
+    cyan
+except NameError:
+    reset = "\033[0m"
+    red = "\033[31m"
+    green = "\033[32m"
+    yellow = "\033[33m"
+    blue = "\033[34m"
+    magenta = "\033[35m"
+    cyan = "\033[36m"
+    white = "\033[37m"
